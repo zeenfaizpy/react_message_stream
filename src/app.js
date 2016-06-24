ae2c381ec999d20fdc0f5f95109718ffb5c66595
Mohammad Faizal <zeenfaiz@gmail.com>
2016-06-25 00:36:24 +0400

    react todo list component added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,48 @@
 import React from 'react'
 
 
-var App = React.createClass({
+var Todo = React.createClass({
     render: function(){
         return (
-            <div>Welcome to {this.props.name}</div>
+            <li>{this.props.name}</li>
         )
     }
 })
 
 
-export default App
+var TodoList = React.createClass({
+    render: function(){
+        var todo_items = this.props.todos.map(function(todo){
+            return (
+                <Todo key={todo.id} name={todo.name} />
+            )
+        })
+        return (
+            <div>
+                <ul>
+                    {todo_items}
+                </ul>
+            </div>
+        )
+    }
+})
+
+export var App = React.createClass({
+    render: function(){
+        var todos = [
+            {
+                'id': 1,
+                'name': 'Item 1',
+            },
+            {
+                'id': 2,
+                'name': 'Item 2',
+            },
+        ]
+        return (
+            <div>
+                <TodoList todos={todos} />
+            </div>
+        )
+    }
+})
