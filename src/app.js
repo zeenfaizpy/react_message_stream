ee696231b54ffd568d18c08fdf9a119974d1f6da
Mohammad Faizal <zeenfaiz@gmail.com>
2016-06-25 02:30:05 +0400

    added more featues for todo list

--- src/app.js
+++ src/app.js
@@ -1,47 +1,81 @@
 import React from 'react'
 
 
-var Todo = React.createClass({
-    render: function(){
-        return (
-            <li>{this.props.name}</li>
-        )
-    }
-})
+var Todo = function(props){
+    return (
+        <li>{props.name}</li>
+    )
+}
 
 
 var TodoList = React.createClass({
     render: function(){
-        var todo_items = this.props.todos.map(function(todo){
-            return (
-                <Todo key={todo.id} name={todo.name} />
-            )
-        })
         return (
             <div>
                 <ul>
-                    {todo_items}
+                    {this.props.todos.map(function(todo){
+                        return (
+                            <Todo key={todo.id} name={todo.name} />
+                        )
+                    })}
                 </ul>
             </div>
         )
     }
 })
 
-export var App = React.createClass({
+var Form = React.createClass({
+    getInitialState: function(){
+        return {
+            'name': '',
+        }
+    },
+    handleClick: function(e){
+        this.props.onFormSubmit({'name': this.state.name});
+        this.setState({'name': ''});
+    },
+    handleChange: function(e){
+        this.setState({'name': e.target.value});
+    },
     render: function(){
-        var todos = [
-            {
-                'id': 1,
-                'name': 'Item 1',
-            },
-            {
-                'id': 2,
-                'name': 'Item 2',
-            },
-        ]
         return (
             <div>
-                <TodoList todos={todos} />
+                <input type="text" name="todo_item" onChange={this.handleChange} value={this.state.name} />
+                <button name="add" onClick={this.handleClick}>Add</button>
+            </div>
+        )
+    }
+})
+
+export var App = React.createClass({
+    getInitialState: function(){
+        return {
+            todos: [
+                {
+                    'id': 1,
+                    'name': 'Item 1',
+                },
+                {
+                    'id': 2,
+                    'name': 'Item 2',
+                }
+            ]
+        }
+    },
+    handleSubmit: function(item){
+        var new_item = {
+            'id': this.state.todos.length + 1,
+            'name': item.name
+        }
+        var all_items = this.state.todos.concat([new_item])
+        this.setState({todos: all_items})
+    },
+    render: function(){
+        
+        return (
+            <div>
+                <Form onFormSubmit={this.handleSubmit} />
+                <TodoList todos={this.state.todos} />
             </div>
         )
     }
